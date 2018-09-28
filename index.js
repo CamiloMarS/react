99263a6b727a7da443db4cada26dbd19cf5b9c11
CamiloMarS <camilo97sdt@gmail.com>
2018-09-28 11:47:47 -0500

    feat: react added to project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import React from "react"; //para crear componentes
+import ReactDOM from "react-dom"; //para renderizar los componentes
+
+/**
+ * Acceder al elemento donde se puntará */
+const app = document.querySelector("#app");
+
+/**
+ * Elementos componente principal
+ */
+const mainComponent = <h1>Hola mundo!!</h1>;
+
+//Recibe dos parametros(que voy a renderizar, donde lo haré)
+ReactDOM.render(mainComponent, app);
