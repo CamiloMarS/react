4fdb7fe3e8cb34abec5065ffff705783acbf831c
CamiloMarS <camilo97sdt@gmail.com>
2018-10-03 13:06:23 -0500

    feat: playlist component created

--- src/playlist/components/playlist/index.js
+++ src/playlist/components/playlist/index.js
@@ -1,35 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Media from "../media";
 import "./playlist.css";
+import Play from "../../../icons/components/play";
 
-class PlayList extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist;
-    console.log(playlist);
-    return (
-      <div className="Playlist">
-        {playlist.map(li => {
-          return <Media {...li} key={li.id} />;
-        })}
-      </div>
-    );
-  }
+function PlayList(props) {
+  const playlist = props.data.categories[0].playlist;
+  console.log(playlist);
+  return (
+    <div className="Playlist">
+      <Play size={50} color="red" />
+      {playlist.map(li => {
+        return <Media {...li} key={li.id} />;
+      })}
+    </div>
+  );
 }
 
 export default PlayList;
-
-/**
- * author: "LeonidasEsteban"
-cover: "./images/covers/optimizar.jpg"
-id: 2
-src: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
-title: "Cómo optimzar la carga de un website"
-type: "video"
-
-<Media
-          image="http://eslamoda.com/wp-content/uploads/sites/2/2017/08/ropa-casual.png"
-          title="¿Qué es Responsive Design?"
-          author="Camilo Mar Salazar"
-          type="video"
-        />
- */
